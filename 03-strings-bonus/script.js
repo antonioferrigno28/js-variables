fab9c00d41a8009df47c0cfd4e2b7b11bab175d1
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-11 15:54:13 +0200

    Done! (Fixed const cityName)

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -1,5 +1,5 @@
 const city = "New York"; // NON MODIFICARE QUESTA RIGA
-const cityName = "Città"; // NON MODIFICARE QUESTA RIGA
+const cityName = "City"; // NON MODIFICARE QUESTA RIGA
 const shoppingList = "mele, banane, arance, uva"; // NON MODIFICARE QUESTA RIGA
 const hello = "hello"; // NON MODIFICARE QUESTA RIGA
 
@@ -22,14 +22,10 @@ console.log(cityReplaced); // Output:  "New Delhi"
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = `${city} ${"City"}`;
-
-//ALTRIMENTI
-//const cityNameConcat = `${city} City`;
-
+const cityNameConcat = `${city} ${cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
-const upperCaseHello = "hello".toUpperCase();
+const upperCaseHello = hello.toUpperCase();
 console.log(upperCaseHello); // Output: "HELLO"
